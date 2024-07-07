ac07a2c5c6aa99355ac0a5ff4a28b9f6f0f4b264
Victor Barbosa <victordfb@gmail.com>
2024-07-07 10:36:20 -0300

    Improvements

--- core/src/main/kotlin/com/smartfuturelabs/utils/CRC.kt
+++ core/src/main/kotlin/com/smartfuturelabs/utils/CRC.kt
@@ -2,18 +2,16 @@ package com.smartfuturelabs.utils
 
 fun crc16(byteArray: ByteArray): Int {
     val polynomial = 0x1021
-    var result = 0xFFFF
-    val bytes = byteArray
-    for (b in bytes) {
+    var output = 0xFFFF
+    for (byte in byteArray) {
         for (i in 0..7) {
-            val bit = (b.toInt() shr 7 - i and 1) == 1
-            val c15 = (result shr 15 and 1) == 1
-            result = result shl 1
+            val bit = (byte.toInt() shr 7 - i and 1) == 1
+            val c15 = (output shr 15 and 1) == 1
+            output = output shl 1
             if (c15 xor bit) {
-                result = result xor polynomial
+                output = output xor polynomial
             }
         }
     }
-    result = result and 0xffff
-    return result
+    return output and 0xffff
 }
